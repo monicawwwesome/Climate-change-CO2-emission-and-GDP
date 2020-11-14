8f00e05a4ec24d967168c8de45ad17c9e5a1fe72
Monica <monicaawang.1997@gmail.com>
2020-11-14 13:02:34 -0800

    Create Climate.py

---
+++ Climate.py
@@ -0,0 +1,24 @@
+#import packages
+
+#import dataset
+
+#Country of most/least GHGs
+'''
+We can find the max/min of the CO2 emission from the "CO2Emission" csv file. 
+Choose the 20(?) countries with the highest/lowest value.
+Then make line graph of the max/min Co2 emission value for each year.
+
+'''
+
+#Country of most/least GHGs per capita
+'''
+We can find the max/min of the CO2 emission per capita from the "CO2POP" csv file. 
+Choose the 20(?) countries with the highest/lowest value.
+Then make line graph of the max/min Co2 emission pre capita value for each year.
+Compare them to the total CO2 emission result.
+'''
+
+
+#Country median vs mean
+
+#Country GHGs/GDP
